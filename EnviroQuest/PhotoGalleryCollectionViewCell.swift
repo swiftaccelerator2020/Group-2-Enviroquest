06752c58425f284427f4b9629d3d5c7c2ead279c
Rachel <rachelgohre@gmail.com>
2020-11-28 21:05:40 +0800

    Hello there is a SIGABRT here Everything else is fine though. Created the photo Gallery View controller Now we need to debug + create the doneWithQuests page uwu

---
+++ EnviroQuest/PhotoGalleryCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoGalleryCollectionViewCell.swift
+//  EnviroQuest
+//
+//  Created by Rachel Goh on 28/11/20.
+//
+
+import UIKit
+
+class PhotoGalleryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var photoGalleryView: UIImageView!
+}
