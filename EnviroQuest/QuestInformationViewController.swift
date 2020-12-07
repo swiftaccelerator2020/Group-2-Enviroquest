9740c51ad6fc3f0eea0c31f21e77ed2dc00cebe1
Rachel <rachelgohre@gmail.com>
2020-12-07 15:57:14 +0800

    ughhhhHHHHHHH - we need to figure out how to let user upload image - saving data... transferring data... HOW design work I finished - autolayout - creating the screens + links and... UGHhHhHhHHHhHhH

---
+++ EnviroQuest/QuestInformationViewController.swift
@@ -0,0 +1,44 @@
+//
+//  QuestInformationViewController.swift
+//  EnviroQuest
+//
+//  Created by Rachel Goh on 7/12/20.
+//
+
+import UIKit
+
+class QuestInformationViewController: UIViewController {
+    var questDescription:String!
+    var questTitle:String!
+    @IBOutlet weak var thirdAnnoyingImageIThink: UIImageView!
+    @IBOutlet weak var theBackgroundView: UIView!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var questDecriptionLabel: UILabel!
+    @IBOutlet weak var questTitleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        thirdAnnoyingImageIThink.contentMode = .scaleAspectFill
+        theBackgroundView.layer.cornerRadius = 20
+        doneButton.layer.cornerRadius = 20
+        doneButton.layer.backgroundColor = .init(red: 1, green: 1, blue: 1, alpha: 1)
+        questTitleLabel.text = questTitle
+        questDecriptionLabel.text = questDescription
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "confirmDone", sender: nil)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
